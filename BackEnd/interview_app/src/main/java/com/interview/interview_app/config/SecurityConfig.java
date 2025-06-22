466d5fddd12eac30995ab5d07873e2b48973a13c
Najeebikka <172572706+Najeebikka@users.noreply.github.com>
2025-06-23 03:18:10 +0530

    changed in security config

--- BackEnd/interview_app/src/main/java/com/interview/interview_app/config/SecurityConfig.java
+++ BackEnd/interview_app/src/main/java/com/interview/interview_app/config/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost"));
+        config.setAllowedOrigins(List.of("http://13.60.35.109"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
