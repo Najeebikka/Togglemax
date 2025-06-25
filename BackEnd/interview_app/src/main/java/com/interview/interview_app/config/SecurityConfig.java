779026adbb1d5abd334172b93f2e53bf82304698
Najeebikka <172572706+Najeebikka@users.noreply.github.com>
2025-06-25 07:21:21 +0530

    Update SecurityConfig.java

--- BackEnd/interview_app/src/main/java/com/interview/interview_app/config/SecurityConfig.java
+++ BackEnd/interview_app/src/main/java/com/interview/interview_app/config/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://13.60.35.109:3000"));
+        config.setAllowedOrigins(List.of("https://13.60.35.109"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
